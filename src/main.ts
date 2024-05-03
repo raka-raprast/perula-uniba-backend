04a7f2ced63136596eb541962f88554bd245e16f
raka-pikpo <raka@pikpo.com>
2024-05-03 13:44:31 +0800

    remove hostname

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT, '0,0,0,0');
+  await app.listen(process.env.PORT);
 }
 bootstrap();
